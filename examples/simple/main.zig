9ee612e4f5529ccb1f95cd3dba33d828cb6657cb
kj4tmp@gmail.com <kj4tmp@gmail.com>
2024-11-09 14:07:50 -0800

    WIP el7041

--- examples/simple/main.zig
+++ examples/simple/main.zig
@@ -46,32 +46,32 @@ const eni = gcat.ENI{
             .outputs_bit_length = 8,
         },
 
-        // .{
-        //     .identity = beckhoff_EL7041_1000,
-        //     .ring_position = 4,
-        //     .inputs_bit_length = 64,
-        //     .outputs_bit_length = 64,
-        //     .coe_startup_parameters = &.{
-        //         .{
-        //             .transition = .PS,
-        //             .direction = .write,
-        //             .index = 0x1c12, // RxPDO Assign
-        //             .subindex = 0x0,
-        //             .complete_access = true,
-        //             .data = &.{ 0x03, 0x00, 0x00, 0x16, 0x02, 0x16, 0x04, 0x16 },
-        //             .timeout_us = 10_000,
-        //         },
-        //         .{
-        //             .transition = .PS,
-        //             .direction = .write,
-        //             .index = 0x1c13, // TxPDO Assign
-        //             .subindex = 0x0,
-        //             .complete_access = true,
-        //             .data = &.{ 0x02, 0x00, 0x00, 0x1a, 0x03, 0x1a },
-        //             .timeout_us = 10_000,
-        //         },
-        //     },
-        // },
+        .{
+            .identity = beckhoff_EL7041_1000,
+            .ring_position = 4,
+            .inputs_bit_length = 64,
+            .outputs_bit_length = 64,
+            .coe_startup_parameters = &.{
+                .{
+                    .transition = .PS,
+                    .direction = .write,
+                    .index = 0x1c12, // RxPDO Assign
+                    .subindex = 0x0,
+                    .complete_access = true,
+                    .data = &.{ 0x03, 0x00, 0x00, 0x16, 0x02, 0x16, 0x04, 0x16 },
+                    .timeout_us = 10_000,
+                },
+                .{
+                    .transition = .PS,
+                    .direction = .write,
+                    .index = 0x1c13, // TxPDO Assign
+                    .subindex = 0x0,
+                    .complete_access = true,
+                    .data = &.{ 0x02, 0x00, 0x00, 0x1a, 0x03, 0x1a },
+                    .timeout_us = 10_000,
+                },
+            },
+        },
     },
 };
 
@@ -106,6 +106,7 @@ pub fn main() !void {
     var timer3 = try std.time.Timer.start();
     var timer4 = try std.time.Timer.start();
     var timer5 = try std.time.Timer.start();
+    var wkc_error_timer = try std.time.Timer.start();
     var frame_count: u32 = 0;
 
     while (true) {
@@ -116,8 +117,10 @@ pub fn main() !void {
                 continue;
             },
             error.Wkc => {
-                std.log.warn("wkc error", .{});
-                continue;
+                if (wkc_error_timer.read() > 1 * std.time.ns_per_s) {
+                    wkc_error_timer.reset();
+                    std.log.warn("wkc error", .{});
+                }
             },
             // error.Wkc,
             error.LinkError,
@@ -145,6 +148,11 @@ pub fn main() !void {
             const reader2 = fbs2.reader();
             std.log.warn("el3314: {}", .{(try gcat.wire.packFromECatReader(EL3314ProcessData, reader2)).ch1});
             frame_count = 0;
+
+            // write to el7041
+            var fb3 = std.io.fixedBufferStream(subdevices[4].runtime_info.pi.?.outputs);
+            const writer3 = fb3.writer();
+            try gcat.wire.eCatFromPackToWriter(EL7041Outputs.enabled, writer3);
         }
 
         if (timer2.read() > std.time.ns_per_s * 0.1) {
@@ -182,4 +190,30 @@ const EL3314ProcessData = packed struct {
     ch2: EL3314Channel,
     ch3: EL3314Channel,
     ch4: EL3314Channel,
+};
+
+const EL7041Outputs = packed struct(u64) {
+    control_enable_latch_c: bool,
+    control_enable_latch_extern_pos_edge: bool,
+    control_set_counter: bool,
+    control_enable_latch_extern_neg_edge: bool,
+    reserved: u12 = 0,
+    set_counter_value: u16,
+    control_enable: bool,
+    control_reset: bool,
+    control_reduce_torque: bool,
+    reserved2: u13 = 0,
+    velocity: i16,
+
+    const enabled = EL7041Outputs{
+        .control_enable = true,
+        .control_enable_latch_c = false,
+        .control_enable_latch_extern_neg_edge = false,
+        .control_enable_latch_extern_pos_edge = false,
+        .control_reduce_torque = true,
+        .control_reset = false,
+        .control_set_counter = false,
+        .set_counter_value = 0,
+        .velocity = 0,
+    };
 };

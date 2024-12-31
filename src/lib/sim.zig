29b0061deb54fc32cba659dcdc1c6a653b60991a
kj4tmp@gmail.com <kj4tmp@gmail.com>
2024-12-30 22:37:01 -0800

    WIP sim

--- src/lib/sim.zig
+++ src/lib/sim.zig
@@ -1,6 +1,7 @@
 //! Deterministic Simulator for EtherCAT Networks
 
 const std = @import("std");
+const assert = std.debug.assert;
 
 const ENI = @import("ENI.zig");
 const nic = @import("nic.zig");
@@ -13,7 +14,30 @@ pub const Simulator = struct {
             .eni = eni,
         };
     }
-    pub fn tick() void {}
+    pub fn tick(self: *Simulator) void {
+        _ = self;
+    }
+
+    pub fn send(ctx: *anyopaque, bytes: []const u8) std.posix.SendError!void {
+        const self: *Simulator = @ptrCast(@alignCast(ctx));
+        _ = self;
+        _ = bytes;
+        unreachable;
+    }
+
+    pub fn recv(ctx: *anyopaque, out: []u8) std.posix.RecvFromError!usize {
+        const self: *Simulator = @ptrCast(@alignCast(ctx));
+        _ = self;
+        _ = out;
+        unreachable;
+    }
+
+    pub fn linkLayer(self: *Simulator) nic.LinkLayer {
+        return nic.LinkLayer{
+            .ptr = self,
+            .vtable = &.{ .send = send, .recv = recv },
+        };
+    }
 };
 
 test {
